8f3f601232806f410ad0af206f075f811a382af3
Alex Spencer <alex@alexspencer.net>
2015-07-13 09:40:23 -0400

    cleanup

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,19 +2,4 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def destroy
-  end
 end
--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -3,13 +3,24 @@ require "rails_helper"
 feature "user signs in" do
   scenario "with good credentials" do
     user = create(:user)
-    
+
+    attempt_login(user)
+    expect(page).to have_content("Signed in successfully.")
+  end
+
+  scenario "with bad credentials" do
+    user = create(:user)
+    user.password = ''
+
+    attempt_login(user)
+    expect(page).to have_content("Invalid email or password.")
+  end
+
+  def attempt_login(user)
     visit '/'
     click_link "Login"
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_button "Log in"
-
-    expect(page).to have_content("Signed in successfully.")
   end
 end
