811feb530a5037c0b0f8048386b3d155c21c4d82
Alex Spencer <alex@alexspencer.net>
2015-07-14 11:26:01 -0400

    Add user->item relationship

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,19 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def update
+    @item = Item.find(params[:item][:id])
+    @item.update_attributes(item_params)
+
+    if @item.save
+      redirect_to :index, notice: 'Congrats, that item is yours!'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:user_id)
+  end
 end
